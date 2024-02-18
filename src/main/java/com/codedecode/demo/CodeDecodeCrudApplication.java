9b8a4060cadd0b5a54b6f0ac20840a83b2344976
unknown <abhishekthawale123@gmail.com>
2024-02-18 15:06:22 +0530

    added Crud

---
+++ src/main/java/com/codedecode/demo/CodeDecodeCrudApplication.java
@@ -0,0 +1,14 @@
+package com.codedecode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDecodeCrudApplication {
+
+	public static void main(String[] args) {
+		System.out.println("CRUD");
+		SpringApplication.run(CodeDecodeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/fileuploadingspringbootrestapi/app/CodeDecodeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.fileuploadingspringbootrestapi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDecodeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
